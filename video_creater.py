8d2f8651d95c32f3bc2347409a989a59d5607bd4
Daniel Riege <daniel.riege99@gmail.com>
2020-05-06 14:35:28 +0200

    fixed video_creater for macOS

--- video_creater.py
+++ video_creater.py
@@ -4,8 +4,8 @@ import sys
 import cv2
 
 test_run_name = "test"
-if len(sys.argv) > 0:
-    test_run_name = sys.argv[0]
+if len(sys.argv) > 1:
+    test_run_name = sys.argv[1]
 
 img_array = []
 size = (0,0)
@@ -14,10 +14,10 @@ for filename in sorted(glob.glob("./training_data/*.jpg")):
     height, width, layers = img.shape
     size = (width, height)
     img_array.append(img)
-out = cv2.VideoWriter("./training_videos/%s.avi" % (test_run_name),
-                        cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter("./training_videos/%s.mp4" % (test_run_name),
+                        fourcc, 30, size)
 for i in range(len(img_array)):
     out.write(img_array[i])
 out.release()
-
-
