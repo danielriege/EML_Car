b771e4f0d699bc155141f252601af27713a793c5
Daniel Riege <daniel.riege99@gmail.com>
2020-05-17 15:20:22 +0200

    changed LaneNavigator to use tensorflow lite

--- drive_computer/src/LaneNavigator.py
+++ drive_computer/src/LaneNavigator.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
-from tensorflow.keras.models import load_model
-#import matplotlib.image as mpimg
+from tflite_runtime.interpreter import Interpreter
 import cv2
 import numpy as np
+import argparse
 
 class LaneNavigator:
         def __init__(self, model_path):
-            self.model = load_model(model_path)
+            # Load TF lite model
+            self.interpreter = Interpreter(args.model)
+            self.interpreter.allocate_tensors()
+            self.input_details = interpreter.get_input_details()
+            self.output_details = interpreter.get_output_details()
         def predictSteeringAngle(self, image):
             preprocessed = self.__preprocess(image)
             X = np.asarray([preprocessed])
-            steering_angle = self.model.predict(X)[0]
-            return steering_angle
+            self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+            self.interpreter.invoke()
+            output_data = interpreter.get_tensor(self.output_details[0]['index'])
+            
+            return output_data
         def __preprocess(self,image):
             height, _, _ = image.shape
             qheight = int(height/4)
@@ -22,10 +29,15 @@ class LaneNavigator:
             image = cv2.resize(image, (200,66)) # input image size (200,66) Nvidia model
             image = image / 255 # normalizing
             return image
-if __name__ == "__main__":
-    laneNavigator = LaneNavigator("../../CNN/lane_navigation.h5")
-    image_name = "0496_1016_1212.jpg"
-    test_image = cv2.imread("../../training_data/black_dot/%s" % (image_name))
+
+def main():
+    parser = argparse.ArgumentParser( formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--image', help='File name of an image in black_dot folder.', required=True)
+    args = parser.parse_args()
+
+    laneNavigator = LaneNavigator("../../CNN/lane_navigation.tflite")
+    test_image = cv2.imread("../../training_data/black_dot/%s" % (args.image))
     angle = laneNavigator.predictSteeringAngle(test_image)
     print(angle)
-
+if __name__ == "__main__":
+    main()
