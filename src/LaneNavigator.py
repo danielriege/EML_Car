c4cd5153ac91334265a5960d27b6417efb33112f
Daniel Riege <daniel.riege99@gamil.com>
2020-06-09 13:11:54 +0200

    bug fixes for street mode

--- src/LaneNavigator.py
+++ src/LaneNavigator.py
@@ -21,13 +21,13 @@ class LaneNavigator:
             self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
             self.interpreter.invoke()
             output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
-            output_data = (output_data-0.5)*(-1)
-            return np.int((output_data[0]+1.5)*1000)
+           # output_data = (output_data-0.5)*(-1)
+            return np.int((output_data[0]+1.0)*1000)
         def __preprocess(self,image):
             height, _, _ = image.shape
             qheight = int(height/4)
             hheight = int(height/2)
-            image = image[qheight:qheight+hheight,:,:] # cropping so only the middle half is visible
+            image = image[-qheight:,:,:] # cropping so only the middle half is visible
             image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)  # Nvidia model said it is best to use YUV color space
             image = cv2.GaussianBlur(image, (3,3), 0)
             image = cv2.resize(image, (200,66)) # input image size (200,66) Nvidia model
